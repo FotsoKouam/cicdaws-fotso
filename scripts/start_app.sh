5e5e391ac9e1d00deaeda50c5468ba7cd22fa211
Manuel Fotso <fotsom144@gmail.com>
2026-07-03 03:12:53 +0100

    fix: update ALLOWED_HOSTS and nginx server_name with new EC2 IP

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["13.60.48.70"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["13.49.64.48"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
